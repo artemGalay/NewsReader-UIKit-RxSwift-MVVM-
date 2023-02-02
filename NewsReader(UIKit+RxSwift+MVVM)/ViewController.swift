57cce73eab49dcf9d7cc84512b1c42c138b0dc92
Artem Galay <iostema@mail.ru>
2023-02-02 15:06:35 +0300

    Initial Commit

---
+++ NewsReader(UIKit+RxSwift+MVVM)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReader(UIKit+RxSwift+MVVM)
+//
+//  Created by Артем Галай on 2.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
